cd6c0179bc39bbc6ea78883b34d0fe11b810bd2f
Desu Sai Venkat <saivenkatdesu@gmail.com>
2022-09-30 10:27:30 +0530

    Initial changes for the Braze Flutter Device Mode

---
+++ integrations/rudder_integration_braze_flutter/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1-dev.1
+
+* TODO: Describe initial release.
--- integrations/rudder_integration_braze_flutter/ios/Classes/RudderIntegrationBrazeFlutterPlugin.m
+++ integrations/rudder_integration_braze_flutter/ios/Classes/RudderIntegrationBrazeFlutterPlugin.m
@@ -1,19 +1,19 @@
-#import "RudderIntegrationAmplitudeFlutterPlugin.h"
-#import "RudderAmplitudeFactory.h"
+#import "RudderIntegrationBrazeFlutterPlugin.h"
+#import "RudderBrazeFactory.h"
 #import <rudder_plugin_ios/RudderSdkFlutterPlugin.h>
 
-@implementation RudderIntegrationAmplitudeFlutterPlugin
+@implementation RudderIntegrationBrazeFlutterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"rudder_integration_amplitude_flutter"
+      methodChannelWithName:@"rudder_integration_braze_flutter"
             binaryMessenger:[registrar messenger]];
-  RudderIntegrationAmplitudeFlutterPlugin* instance = [[RudderIntegrationAmplitudeFlutterPlugin alloc] init];
+  RudderIntegrationBrazeFlutterPlugin* instance = [[RudderIntegrationBrazeFlutterPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"addFactory" isEqualToString:call.method]) {
-     [RudderSdkFlutterPlugin addIntegration:[RudderAmplitudeFactory instance]];
+     [RudderSdkFlutterPlugin addIntegration:[RudderBrazeFactory instance]];
     // To do with result
   } else {
     result(FlutterMethodNotImplemented);
