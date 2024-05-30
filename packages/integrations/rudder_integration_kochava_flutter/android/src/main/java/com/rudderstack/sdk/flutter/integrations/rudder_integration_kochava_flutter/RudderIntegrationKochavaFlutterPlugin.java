8cf93b7d327767099d8dfd36cf39acdbf1eb8a30
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2024-05-31 00:17:28 +0530

    feat: implement kochava sdk initialisation in the android module

--- packages/integrations/rudder_integration_kochava_flutter/android/src/main/java/com/rudderstack/sdk/flutter/integrations/rudder_integration_kochava_flutter/RudderIntegrationKochavaFlutterPlugin.java
+++ packages/integrations/rudder_integration_kochava_flutter/android/src/main/java/com/rudderstack/sdk/flutter/integrations/rudder_integration_kochava_flutter/RudderIntegrationKochavaFlutterPlugin.java
@@ -2,8 +2,19 @@ package com.rudderstack.sdk.flutter.integrations.rudder_integration_kochava_flut
 
 import static com.rudderstack.sdk.flutter.RudderSdkFlutterPlugin.addIntegration;
 
+import android.content.Context;
+import android.text.TextUtils;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.kochava.tracker.Tracker;
+import com.kochava.tracker.TrackerApi;
+import com.kochava.tracker.log.LogLevel;
 import com.rudderstack.android.integrations.kochava.KochavaIntegrationFactory;
+
+import java.util.HashMap;
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
@@ -17,9 +28,13 @@ public class RudderIntegrationKochavaFlutterPlugin implements FlutterPlugin, Met
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Context context;
 
+
+  // .
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    context = flutterPluginBinding.getApplicationContext();
     channel =
         new MethodChannel(
             flutterPluginBinding.getBinaryMessenger(), "rudder_integration_kochava_flutter");
@@ -30,9 +45,49 @@ public class RudderIntegrationKochavaFlutterPlugin implements FlutterPlugin, Met
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("addFactory")) {
       addIntegration(KochavaIntegrationFactory.FACTORY);
-      // To do with result
+    } else if (call.method.equals("initialiseKochavaSDK")) {
+      initialiseKochavaSDK(call);
     } else {
       result.notImplemented();
+    }
+  }
+
+  private void initialiseKochavaSDK(@NonNull MethodCall call) {
+    HashMap<String, Object> argumentsMap = (HashMap<String, Object>) call.arguments;
+    if (argumentsMap != null && argumentsMap.containsKey("guid")) {
+      String kochavaGuid = (String) argumentsMap.get("guid");
+      if (!TextUtils.isEmpty(kochavaGuid)) {
+        TrackerApi kochavaInstance  = Tracker.getInstance();
+        String logLevel = (String) argumentsMap.get("logLevel");
+        setLogLevel(logLevel, kochavaInstance);
+        kochavaInstance.startWithAppGuid(context, kochavaGuid);
+      }
+    }
+  }
+
+  private void setLogLevel(@Nullable String logLevel, TrackerApi kochavaInstance) {
+    if (logLevel == null) {
+      kochavaInstance.setLogLevel(LogLevel.NONE);
+      return;
+    }
+    switch (logLevel.toUpperCase()) {
+      case "TRACE":
+        kochavaInstance.setLogLevel(LogLevel.TRACE);
+        break;
+      case "DEBUG":
+        kochavaInstance.setLogLevel(LogLevel.DEBUG);
+        break;
+      case "INFO":
+        kochavaInstance.setLogLevel(LogLevel.INFO);
+        break;
+      case "WARN":
+        kochavaInstance.setLogLevel(LogLevel.WARN);
+        break;
+      case "ERROR":
+        kochavaInstance.setLogLevel(LogLevel.ERROR);
+        break;
+      default:
+        kochavaInstance.setLogLevel(LogLevel.NONE);
     }
   }
 
--- packages/integrations/rudder_integration_kochava_flutter/lib/rudder_integration_kochava_flutter.dart
+++ packages/integrations/rudder_integration_kochava_flutter/lib/rudder_integration_kochava_flutter.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
+import './rudder_integration_kochava_flutter_enum.dart';
+export './rudder_integration_kochava_flutter_enum.dart';
 
 class RudderIntegrationKochavaFlutter implements RudderIntegration {
   static const MethodChannel _channel =
@@ -11,6 +13,14 @@ class RudderIntegrationKochavaFlutter implements RudderIntegration {
     if (!kIsWeb) {
       _channel.invokeMethod("addFactory");
     }
+  }
+
+  static void initialiseKochavaSDK(String guid, RudderKochavaLogLevel loglevel) {
+    Map<String, dynamic> params = {};
+
+    params["guid"] = guid;
+    params["logLevel"] = loglevel.toString();
+    _channel.invokeMethod("initialiseKochavaSDK", params);
   }
 
   @override
