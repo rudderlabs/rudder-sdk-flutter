96625304757789301a55b001d96dc98a1da46c4a
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-08-26 18:10:51 +0530

    feat(rudder_plugin_ios): improve package score

---
+++ packages/plugins/rudder_plugin_ios/example/lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: uri_does_not_exist, undefined_class, undefined_function, undefined_identifier, depend_on_referenced_packages
+
+import 'package:rudder_sdk_flutter/RudderController.dart';
+import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
+
+void main() {
+  // Initialize the SDK with your write key and configuration
+  MobileConfig mc = MobileConfig(
+      autoCollectAdvertId: false,
+      sessionTimeoutInMillis: 6000,
+      gzip: false,
+      recordScreenViews: true,
+      collectDeviceId: false);
+
+  RudderConfigBuilder builder = RudderConfigBuilder();
+  builder
+    ..withDataPlaneUrl('your-data-plane-url')
+    ..withMobileConfig(mc)
+    ..withLogLevel(RudderLogger.VERBOSE);
+
+  RudderController.initialize('your-write-key', config: builder.build());
+}
