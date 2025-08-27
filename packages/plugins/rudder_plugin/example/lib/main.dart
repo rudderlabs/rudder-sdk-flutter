8b3f6be8c2a7ef9b7918eceea5e4cc0282c7fd2c
Abhishek Pandey <64667840+1abhishekpandey@users.noreply.github.com>
2025-08-27 16:52:36 +0530

    chore: update import in the example main.dart file

--- packages/plugins/rudder_plugin/example/lib/main.dart
+++ packages/plugins/rudder_plugin/example/lib/main.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: uri_does_not_exist, undefined_class, undefined_function, undefined_identifier, depend_on_referenced_packages
 
 import 'package:rudder_sdk_flutter/RudderController.dart';
+import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
 
 void main() {
   // Initialize the SDK with your write key and configuration
