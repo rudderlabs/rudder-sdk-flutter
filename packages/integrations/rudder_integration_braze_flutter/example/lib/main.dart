48bd644d7de45595da68bbb510a4a6302c091cb9
dciccale <539546+dciccale@users.noreply.github.com>
2026-07-30 15:33:24 +0200

    docs: sdk-3671 initialize flutter in braze example

--- packages/integrations/rudder_integration_braze_flutter/example/lib/main.dart
+++ packages/integrations/rudder_integration_braze_flutter/example/lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/widgets.dart';
 import 'package:rudder_integration_braze_flutter/rudder_integration_braze_flutter.dart';
 import 'package:rudder_sdk_flutter/RudderController.dart';
 import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   final config = RudderConfigBuilder()
     ..withDataPlaneUrl('YOUR_DATA_PLANE_URL')
     ..withFactory(RudderIntegrationBrazeFlutter());
@@ -11,4 +14,6 @@ void main() {
     'YOUR_WRITE_KEY',
     config: config.build(),
   );
+
+  runApp(const SizedBox.shrink());
 }
