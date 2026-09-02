a2c9ef7d72612cf31b19ddca193c559f1dede4a9
dciccale <539546+dciccale@users.noreply.github.com>
2026-09-02 17:32:15 +0200

    docs: sdk-3669 improve appsflyer package score

--- packages/integrations/rudder_integration_appsflyer_flutter/lib/rudder_integration_appsflyer_flutter.dart
+++ packages/integrations/rudder_integration_appsflyer_flutter/lib/rudder_integration_appsflyer_flutter.dart
@@ -2,10 +2,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter/foundation.dart';
 import 'package:rudder_sdk_flutter_platform_interface/platform.dart';
 
+/// Adds AppsFlyer as a device-mode destination to the RudderStack Flutter SDK.
 class RudderIntegrationAppsflyerFlutter implements RudderIntegration {
   static const MethodChannel _channel =
       MethodChannel('rudder_integration_appsflyer_flutter');
 
+  /// Registers the native AppsFlyer integration factory with the RudderStack SDK.
   @override
   void addFactory() {
     if (!kIsWeb) {
@@ -13,6 +15,7 @@ class RudderIntegrationAppsflyerFlutter implements RudderIntegration {
     }
   }
 
+  /// Returns the destination key used to identify the AppsFlyer integration.
   @override
   String getKey() {
     return "AppsFlyer";
